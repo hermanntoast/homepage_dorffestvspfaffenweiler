a2c1ddecc0a5f1e3e6f753d53009c244c48b37fb
hermanntoast <lukas.spitznagel@netzint.de>
2025-10-16 15:06:00 +0200

    Add impressum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Impressum } from './impressum/impressum';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'impressum', component: Impressum }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,56 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit, OnDestroy {
-  days: number = 0;
-  hours: number = 0;
-  minutes: number = 0;
-  seconds: number = 0;
-  private countdownInterval: any;
-
-  ngOnInit() {
-    this.startCountdown();
-  }
-
-  ngOnDestroy() {
-    if (this.countdownInterval) {
-      clearInterval(this.countdownInterval);
-    }
-  }
-
-  private startCountdown() {
-    const targetDate = new Date('2026-06-20T00:00:00').getTime();
-    
-    this.updateCountdown(targetDate);
-    
-    this.countdownInterval = setInterval(() => {
-      this.updateCountdown(targetDate);
-    }, 1000);
-  }
-
-  private updateCountdown(targetDate: number) {
-    const now = new Date().getTime();
-    const distance = targetDate - now;
-
-    if (distance > 0) {
-      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    } else {
-      this.days = 0;
-      this.hours = 0;
-      this.minutes = 0;
-      this.seconds = 0;
-      if (this.countdownInterval) {
-        clearInterval(this.countdownInterval);
-      }
-    }
-  }
+export class App {
 }
